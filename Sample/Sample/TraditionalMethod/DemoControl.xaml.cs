b413219f4e37449f596193710de26cec6ea61a39
robinzevallos <zevallosrobin@gmail.com>
2019-01-23 13:12:32 -0500

    Added full

---
+++ Sample/Sample/TraditionalMethod/DemoControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Sample.Traditional
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DemoControl : ContentView
+	{
+        public static readonly BindableProperty TitleProperty 
+            = BindableProperty.Create(
+                "Title",
+                typeof(String),
+                typeof(DemoControl));
+
+        public String Title
+        {
+            get => (String)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly BindableProperty DescriptionProperty
+            = BindableProperty.Create(
+                "Description",
+                typeof(String),
+                typeof(DemoControl));
+
+        public String Description
+        {
+            get => (String)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly BindableProperty BackgroundColorButtonProperty
+            = BindableProperty.Create(
+                "BackgroundColorButton",
+                typeof(Color),
+                typeof(DemoControl));
+
+        public Color BackgroundColorButton
+        {
+            get => (Color)GetValue(BackgroundColorButtonProperty);
+            set => SetValue(BackgroundColorButtonProperty, value);
+        }
+
+        public DemoControl()
+		{
+		    InitializeComponent();
+
+            BindingContext = this;
+		}
+	}
+}
---
+++ Sample/Sample/With Kasay.BindableProperty/DemoControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Sample
+{
+    using Kasay.BindableProperty;
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DemoControl : ContentView
+	{
+        [Bind] public String Title { get; set; }
+
+        [Bind] public String Description { get; set; }
+
+        [Bind] public Color BackgroundColorButton { get; set; }
+
+        public DemoControl()
+		{
+			InitializeComponent();
+        }
+	}
+}
